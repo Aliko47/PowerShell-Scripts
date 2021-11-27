16ed37bda8dfd95305cc7880f2f927b294983254
Ali <64026099+Aliko47@users.noreply.github.com>
2021-11-27 21:22:34 +0100

    Add files via upload

---
+++ disableCortana.ps1
@@ -0,0 +1,11 @@
+﻿# Set the location to the registry
+Set-Location -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows'
+
+# Create a new Key
+Get-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows' | New-Item -Name 'Windows Search' -Force
+
+# Create new items with values
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search' -Name 'AllowCortana' -Value "0" -PropertyType DWord -Force
+
+# Get out of the Registry
+Pop-Location
