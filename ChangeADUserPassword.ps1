0659df1a5db66d0de3401246a2de95869d4c90ca
Ali <64026099+Aliko47@users.noreply.github.com>
2021-12-24 18:46:11 +0100

    Add files via upload

---
+++ ChangeADUserPassword.ps1
@@ -0,0 +1,16 @@
+﻿Get-Module ActiveDirectory
+
+$user = Read-Host -Prompt 'Input AD-Username'
+$newPassword = Read-Host -Prompt 'Input new password'
+$newPassword = ConvertTo-SecureString $newPassword -AsPlainText -Force
+$logonChangePW = Read-Host 'Change password at next logon (Y/N)?'
+
+#Proceed PW Change at AD
+Set-ADAccountPassword -Identity $user -NewPassword $newPassword -Reset
+
+#Change password at next logon?
+if ($logonChangePW -eq 'y'-or $logonChangePW -eq 'Y' -or $logonChangePW -eq '') {
+    Set-ADUser -Identity $user -ChangePasswordAtLogon $true
+} 
+
+Write-Host "Password changed!"
