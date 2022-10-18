8d31936cefa5dfb7ddf8e218ad4f9b7e76bc0cfd
Ali <64026099+Aliko47@users.noreply.github.com>
2022-10-18 14:33:55 +0200

    Update ChangeADUserPassword.ps1

--- ChangeADUserPassword.ps1
+++ ChangeADUserPassword.ps1
@@ -1,19 +1,48 @@
-#Load AD Module
-Get-Module ActiveDirectory
+#Load AD Module, if RSAT is installed
+#Get-Module ActiveDirectory
 
-#Promp user to type in sAMAccountName
-$user = Read-Host -Prompt 'Input AD-Username'
-$newPassword = Read-Host -Prompt 'Input new password'
-$newPassword = ConvertTo-SecureString $newPassword -AsPlainText -Force
+#Or you can use this one
+Try{
+    #Load AD Module
+    $DC = New-PSSession -ComputerName DC
+    Import-Module -PSsession $DC -Name ActiveDirectory
+}
+Catch{
+    #AD Module failed
+    Write-Host "Loading AD Module failed!" -ForegroundColor Red
+    $PSitem 
+    break
+}
 
-$logonChangePW = Read-Host 'Change password at next logon (Y/N)?'
+do {
 
-#Proceed PW Change at AD
-Set-ADAccountPassword -Identity $user -NewPassword $newPassword -Reset
+    #Get User sAMAccountName
+    $user = Read-Host -Prompt 'Input AD-Username'
+    $newPassword = Read-Host -Prompt 'Input new password' -AsSecureString
+    $ReTypeNewPassword = Read-Host -Prompt 'Retype new password' -AsSecureString
+    #Encrypt password for comparing
+    $newPassword_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($newPassword))
+    $ReTypeNewPassword_text = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($ReTypeNewPassword))
 
-#Change password at next logon?
-if ($logonChangePW -eq 'y'-or $logonChangePW -eq 'Y' -or $logonChangePW -eq '') {
-    Set-ADUser -Identity $user -ChangePasswordAtLogon $true
-} 
+    #Compare password if match
+    if($newPassword_text -ne $ReTypeNewPassword_text){
+        Write-Host "Password does not match! Please retype" -ForegroundColor Red
+    }
+    else {
+        $newPassword = ConvertTo-SecureString $newPassword -AsPlainText -Force
+    }
 
-Write-Host "Password changed!"
+}while($newPassword_text -ne $ReTypeNewPassword_text)
+
+
+Try{
+    #Proceed PW Change at AD
+    Set-ADAccountPassword -Identity $user -NewPassword $newPassword -Reset
+    Write-Host "Password changed!" -ForegroundColor Green
+}
+Catch{
+    #Changing password failed
+    Write-Host "An error occurred" -ForegroundColor Red
+    $PSitem 
+    break
+}
